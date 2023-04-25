99f9e94ddb6dd6267df9edcd8813c83e56994482
patrickarmani <arm22001@byui.edu>
2023-04-25 19:02:06 -0300

    "week 01 - Assignment Portal"

---
+++ scripts/footer.js
@@ -0,0 +1,4 @@
+document.querySelector("#year").innerHTML = "&copy; 2023 | Patrick Armani | Brazil";
+
+let lastUpdated = new Date(document.lastModified);
+document.getElementById("lastUpdated").textContent = lastUpdated;
