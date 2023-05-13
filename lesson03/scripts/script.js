741102c9e7ac5547ed4ac37eb6205943ad0ca892
patrickarmani <arm22001@byui.edu>
2023-05-13 10:00:52 -0300

    "lesson3"

---
+++ lesson03/scripts/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#year").innerHTML = "&copy; 2023 | WDD230 - BYU - Idaho | Patrick Armani | Brazil";
+
+let lastUpdated = new Date(document.lastModified);
+document.getElementById("lastUpdated").textContent = lastUpdated;
