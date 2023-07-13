45c3b1e2b5dc9fc42d34a8e1a526b535348fbac3
patrickarmani <arm22001@byui.edu>
2023-07-13 11:30:39 -0300

    "Chamber"

---
+++ chamber/scripts/datetime.js
@@ -0,0 +1,12 @@
+const currentDate = new Date();
+  
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+const year = currentDate.getFullYear();
+const hours = currentDate.getHours();
+const minutes = currentDate.getMinutes();
+const seconds = currentDate.getSeconds();
+
+const formattedDateTime = `${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}-${year} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+document.getElementById('formDateTime').value = formattedDateTime;
